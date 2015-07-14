7e1c72effc229a682c7e633a9e7192b48a32e399
Sasha Aickin <xander76@yahoo.com>
2015-07-13 18:13:31 -0700

    Moved babel compilation to runtime

--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp"),
-	babel = require("gulp-babel");
-
-gulp.task("default", () => {
-	return gulp.src("src/**/*.js")
-		.pipe(babel({stage: 1, optional: ["runtime"]}))
-		.pipe(gulp.dest("target"));
-});
